d3c8fd0cfd00ccc808f09521f9c3b3db69aedf00
Riley Collard <91252297+Ethyx07@users.noreply.github.com>
2025-12-08 14:11:50 +1100

    Implemented glfw and glew
    Implemented glfw and glew libraries

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,45 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+using namespace std;
 
 int main()
 {
-    if (!glfwInit()) 
+    if (!glfwInit()) //Makes sure glfw initialised properly
     {
         return -1;
     }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //tells glfw what version of opengl is being used and sets its profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "GameDevelopmentProject", NULL, NULL); //Creates window terminal
+    if (window == NULL) 
+    {
+        cout << "ERROR CREATING WINDOW" << endl;
+        glfwTerminate(); //Terminates if window is not created
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window); //Sets window as glfw current context
+
+    if (glewInit() != GLEW_OK)  //Checks if glew was initialised properly
+    {
+        glfwTerminate();
+        return -1;
+    }
+ 
+    while (!glfwWindowShouldClose(window))  //Loops until window is prompted to close
+    {
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window); //Swaps buffers between back and front buffer
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
